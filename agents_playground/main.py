ec655fa5b2f903c40033738b44d90d1bca0f4e70
Samuel Holloway <sholloway@gmail.com>
2021-12-03 10:52:43 -0600

    Prototyped launching a new viewport from menu with dedicated animation thread.

--- agents_playground/main.py
+++ agents_playground/main.py
@@ -1,6 +1,6 @@
 import math
 import threading
-from time import sleep
+from time import sleep, perf_counter
 
 import dearpygui.dearpygui as dpg
 
@@ -21,6 +21,36 @@ def animation_thread_loop():
     planet2_moon2angle += 5
     dpg.apply_transform("planet 2, moon 2 node", dpg.create_rotation_matrix(math.pi*planet2_moon2angle/180.0 , [0, 0, -1])*dpg.create_translation_matrix([planet2_moon2distance, 0]))
 
+def pulse(time:float):
+  # frequency: float = 10; # Frequency in Hz
+  return 0.5*(1+math.sin(2 * math.pi * time))
+
+def single_agent_sim_loop():
+  # for now just have the radius pulsate.
+
+  default_radius: float = 20
+  scale:float = 10
+
+  while True:
+    sleep(0.100) 
+    inflat:float = pulse(perf_counter())
+    new_radius = default_radius + scale * inflat
+    dpg.configure_item('my-circle',radius = new_radius)
+
+def launch_single_agent_sim(sender):
+  with dpg.window(label="Single Agent Simulation", width=400, height=400):
+    dpg.add_text("Baby steps...")
+    # Create a drawing canvas
+    with dpg.drawlist(label="My Drawlist", width=400, height=400): 
+      with dpg.draw_node(label="My Node", tag="circle-node"):
+        dpg.draw_circle(tag="my-circle", center = [0,0], radius=20, color=[0, 0, 0], fill=[0, 0, 255])
+  
+    dpg.apply_transform("circle-node", dpg.create_translation_matrix([250, 250]))  
+
+  # Create a thread for updating the simulation.
+  sim_thread = threading.Thread(name="single-agent-thread", target=single_agent_sim_loop, args=(), daemon=True)
+  sim_thread.start()
+
 @log
 def setup_ui():
   dpg.create_context()
@@ -30,6 +60,12 @@ def setup_ui():
     dpg.add_text("Hello, world")
 
   dpg.create_viewport(title="Intelligent Agent Playground")
+
+  # Configure the primary window's menu bar.
+  with dpg.viewport_menu_bar():
+    with dpg.menu(label="Simulations"):
+      dpg.add_menu_item(label="Single Agent", callback=launch_single_agent_sim)
+      
   dpg.setup_dearpygui() # Assign the viewport
 
   # This is a nested window.
