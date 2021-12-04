e9c77162755fd3e63d3a05683ff547e797fbad41
Samuel Holloway <sholloway@gmail.com>
2021-12-04 16:24:24 -0600

    Divided the rendering setup into two functions. One for the initial render, the other for when the sim has launched.

--- agents_playground/single_agent_simulation.py
+++ agents_playground/single_agent_simulation.py
@@ -26,6 +26,8 @@ Questions
 """
 RUN_SIM_TOGGLE_BTN_START_LABEL = 'Start'
 RUN_SIM_TOGGLE_BTN_STOP_LABEL = 'Stop'
+SIM_DESCRIPTION = 'Single agent simulation of an agent following a predefined path.'
+SIM_INSTRUCTIONS = 'Click the start button to begin the simulation.'
 
 # TODO: Move to simulation.py
 class SimulationState(Enum):
@@ -49,6 +51,8 @@ class SingleAgentSimulation(Simulation):
   def __init__(self) -> None:
     super().__init__()
     self._sim_current_state: SimulationState = SimulationState.INITIAL
+    self._sim_window_ref = dpg.generate_uuid()
+    self._sim_initial_state_dl_ref = dpg.generate_uuid()
     self._buttons = {
       'sim': {
         'run_sim_toggle_btn': dpg.generate_uuid()
@@ -94,85 +98,17 @@ class SingleAgentSimulation(Simulation):
   def launch(self):
     parent_width: Optional[int] = dpg.get_item_width(super().primary_window())
     parent_height: Optional[int]  = dpg.get_item_height(super().primary_window())
-    canvas_width: int = parent_width if parent_width else 0
-    canvas_height: int = parent_height - 40 if parent_height else 0
 
-    rows: int = math.floor(canvas_height/self._cell_height) - 1
-    columns: int = math.floor(canvas_width/self._cell_width) - 1
-    grid_background_color = (255,255,255)
-    grid_width = columns*self._cell_width
-    grid_height = rows*self._cell_height
-    grid_line_color = (0, 0, 0)
-    grid_line_thickness: float = 1
-
-    # Agent stuff
-    agent_stroke_thickness: float = 1.0
-    agent_stroke_color = (255,255,255)
-    agent_fill_color = (0, 0, 255)
-    agent_width: float = 20
-    agent_height: float = 20
-    agent_width_half: float = agent_width/2
-    agent_height_half: float = agent_height/2
-
-    with dpg.window(label=self._title, width=parent_width, height=parent_height, on_close=self._handle_sim_closed):
-      with dpg.menu_bar():
-        dpg.add_button(label=RUN_SIM_TOGGLE_BTN_START_LABEL, tag=self._buttons['sim']['run_sim_toggle_btn'], callback=self._run_sim_toggle_btn_clicked)
-        with dpg.menu(label="Display"):
-          dpg.add_menu_item(
-            label="Terrain", 
-            callback=self._toggle_layer, 
-            tag=self.menu_items['display']['terrain'], 
-            check=True, 
-            default_value=True, 
-            user_data=self._layers['terrain'])
-          dpg.add_menu_item(
-            label="Path", 
-            callback=self._toggle_layer, 
-            tag=self.menu_items['display']['path'], 
-            check=True, 
-            default_value=True, 
-            user_data=self._layers['path'])
-
-      with dpg.drawlist(width=canvas_width, height=canvas_height): 
-        with dpg.draw_layer(tag=self._layers['terrain']): 
-          dpg.draw_rectangle(pmin=(0,0),pmax=(grid_width,grid_height), fill=grid_background_color)
-          # For right now, just draw a grid.
-          for row in range(rows + 1): # Draw horizontal lines
-            vertical_offset = row * self._cell_height
-            dpg.draw_line(p1=(0, vertical_offset), p2=(grid_width, vertical_offset), color=grid_line_color, thickness=grid_line_thickness)
-
-          for col in range(columns+1): # Draw vertical lines
-            horizontal_offset = col * self._cell_width
-            dpg.draw_line(p1=(horizontal_offset, 0), p2=(horizontal_offset, grid_height), color=grid_line_color, thickness=grid_line_thickness)
-
-        with dpg.draw_layer(tag=self._layers['navigation_mesh']): # navigation mesh?
-          pass
-
-        with dpg.draw_layer(tag=self._layers['path']): # Path
-          # Transform the path of cells into canvas points.
-          displayed_path = []
-          for step in self._path:
-            point = [
-              step[0] * self._cell_width + self._cell_center_x_offset, 
-              step[1] * self._cell_height + self._cell_center_y_offset
-            ]
-            displayed_path.append(point)
-          dpg.draw_polyline(displayed_path, closed=True, color=(255,0,0))
-
-        with dpg.draw_layer(tag=self._layers['agents']): # Agents
-          with dpg.draw_node(tag=self._agent_ref):
-            # Draw the triangle centered at cell (0,0) in the grid.
-            dpg.draw_triangle(
-              p1=(agent_width_half,0), 
-              p2=(0, agent_height), 
-              p3=(agent_width, agent_height), 
-              color=agent_stroke_color, 
-              fill=agent_fill_color, 
-              thickness=agent_stroke_thickness)
-
-    # Create a thread for updating the simulation.
-    sim_thread = threading.Thread(name="single-agent-thread", target=self._sim_loop, args=(), daemon=True)
-    sim_thread.start()
+    with dpg.window(tag=self._sim_window_ref, 
+      label=self._title, 
+      width=parent_width, 
+      height=parent_height, 
+      on_close=self._handle_sim_closed):
+      self._setup_menu_bar()
+      if self._sim_current_state is SimulationState.INITIAL:
+        self._initial_render()
+      else:
+        self._bootstrap_simulation_render()
 
   def _handle_sim_closed(self, sender, app_data, user_data):
     super().notify(SimulationEvents.WINDOW_CLOSED.value)
@@ -202,5 +138,104 @@ class SingleAgentSimulation(Simulation):
   def _run_sim_toggle_btn_clicked(self, sender, item_data, user_data ):
     next_state: SimulationState = SimulationStateTable[self._sim_current_state]
     next_label: str = SimulationStateToLabelMap[next_state]
-    self._sim_current_state = next_state
     dpg.set_item_label(sender, next_label)
+    
+    if self._sim_current_state is SimulationState.INITIAL:
+      # special case for starting the simulation for the first time.
+      dpg.delete_item(self._sim_initial_state_dl_ref)
+      self._bootstrap_simulation_render()
+    
+    self._sim_current_state = next_state
+
+  def _setup_menu_bar(self):
+    with dpg.menu_bar():
+      dpg.add_button(label=RUN_SIM_TOGGLE_BTN_START_LABEL, tag=self._buttons['sim']['run_sim_toggle_btn'], callback=self._run_sim_toggle_btn_clicked)
+      with dpg.menu(label="Display"):
+        dpg.add_menu_item(
+          label="Terrain", 
+          callback=self._toggle_layer, 
+          tag=self.menu_items['display']['terrain'], 
+          check=True, 
+          default_value=True, 
+          user_data=self._layers['terrain'])
+        dpg.add_menu_item(
+          label="Path", 
+          callback=self._toggle_layer, 
+          tag=self.menu_items['display']['path'], 
+          check=True, 
+          default_value=True, 
+          user_data=self._layers['path'])
+
+  def _initial_render(self):
+    parent_width: Optional[int] = dpg.get_item_width(super().primary_window())
+    parent_height: Optional[int]  = dpg.get_item_height(super().primary_window())
+    canvas_width: int = parent_width if parent_width else 0
+    canvas_height: int = parent_height - 40 if parent_height else 0
+
+    with dpg.drawlist(tag=self._sim_initial_state_dl_ref, parent=self._sim_window_ref, width=canvas_width, height=canvas_height): 
+      dpg.draw_text(pos=(20,20), text=SIM_DESCRIPTION, size=20)
+      dpg.draw_text(pos=(20,40), text=SIM_INSTRUCTIONS, size=20)
+
+  def _bootstrap_simulation_render(self):
+    parent_width: Optional[int] = dpg.get_item_width(super().primary_window())
+    parent_height: Optional[int]  = dpg.get_item_height(super().primary_window())
+    canvas_width: int = parent_width if parent_width else 0
+    canvas_height: int = parent_height - 40 if parent_height else 0
+
+    rows: int = math.floor(canvas_height/self._cell_height) - 1
+    columns: int = math.floor(canvas_width/self._cell_width) - 1
+    grid_background_color = (255,255,255)
+    grid_width = columns*self._cell_width
+    grid_height = rows*self._cell_height
+    grid_line_color = (0, 0, 0)
+    grid_line_thickness: float = 1
+
+    # Agent stuff
+    agent_stroke_thickness: float = 1.0
+    agent_stroke_color = (255,255,255)
+    agent_fill_color = (0, 0, 255)
+    agent_width: float = 20
+    agent_height: float = 20
+    agent_width_half: float = agent_width/2
+    agent_height_half: float = agent_height/2
+
+    with dpg.drawlist(parent=self._sim_window_ref, width=canvas_width, height=canvas_height): 
+      with dpg.draw_layer(tag=self._layers['terrain']): 
+        dpg.draw_rectangle(pmin=(0,0),pmax=(grid_width,grid_height), fill=grid_background_color)
+        # For right now, just draw a grid.
+        for row in range(rows + 1): # Draw horizontal lines
+          vertical_offset = row * self._cell_height
+          dpg.draw_line(p1=(0, vertical_offset), p2=(grid_width, vertical_offset), color=grid_line_color, thickness=grid_line_thickness)
+
+        for col in range(columns+1): # Draw vertical lines
+          horizontal_offset = col * self._cell_width
+          dpg.draw_line(p1=(horizontal_offset, 0), p2=(horizontal_offset, grid_height), color=grid_line_color, thickness=grid_line_thickness)
+
+      with dpg.draw_layer(tag=self._layers['navigation_mesh']): # navigation mesh?
+        pass
+
+      with dpg.draw_layer(tag=self._layers['path']): # Path
+        # Transform the path of cells into canvas points.
+        displayed_path = []
+        for step in self._path:
+          point = [
+            step[0] * self._cell_width + self._cell_center_x_offset, 
+            step[1] * self._cell_height + self._cell_center_y_offset
+          ]
+          displayed_path.append(point)
+        dpg.draw_polyline(displayed_path, closed=True, color=(255,0,0))
+
+      with dpg.draw_layer(tag=self._layers['agents']): # Agents
+        with dpg.draw_node(tag=self._agent_ref):
+          # Draw the triangle centered at cell (0,0) in the grid.
+          dpg.draw_triangle(
+            p1=(agent_width_half,0), 
+            p2=(0, agent_height), 
+            p3=(agent_width, agent_height), 
+            color=agent_stroke_color, 
+            fill=agent_fill_color, 
+            thickness=agent_stroke_thickness
+          )
+    # Create a thread for updating the simulation.
+    sim_thread = threading.Thread(name="single-agent-thread", target=self._sim_loop, args=(), daemon=True)
+    sim_thread.start()
