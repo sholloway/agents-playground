3774d7ab54ae6b38cad56dd4b37ca953f093798b
Samuel Holloway <sholloway@gmail.com>
2021-12-09 07:08:38 -0600

    Cleaned up the agent animation code a bit.

--- agents_playground/single_agent_simulation.py
+++ agents_playground/single_agent_simulation.py
@@ -1,11 +1,12 @@
 import math
 
 from time import sleep
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 import dearpygui.dearpygui as dpg
 
 from agents_playground.agent import Agent
+from agents_playground.direction import Direction, DIR_ROTATION
 from agents_playground.logger import log, get_default_logger
 from agents_playground.simulation import (
   Simulation, 
@@ -27,8 +28,10 @@ What do I want here?
 - [X] Pull boilerplate into simulation.py
 - [X] Update to actually use the Agent definition.
 - [X] Associate the stepping of the path with the agent.
-- [ ] Define path abstraction
+- [X] Define path abstraction
 - [ ] Make the triangle rotates when changing direction.
+  The triangle is rotating around 0,0. This is displacing it from what it should be.
+  Need to rotate around the center of the triangle. Shift the triangle to be centered on 0,0.
 - [ ] Unit Tests
 - [ ] some kind of landscape with obsticles to navigate.
   - Perhaps there are different "maps" that can be selected via a combo box or menu.
@@ -43,9 +46,9 @@ SIM_INSTRUCTIONs = 'Click the start button to begin the simulation.'
 
 Color = Tuple[int, int, int]
 
-def s(x: int, y: int) -> AgentStep:
+def s(x: int, y: int, dir: Optional[Direction] = None) -> AgentStep:
   """Convenance function for building a path step"""
-  return AgentStep(Point(x,y))
+  return AgentStep(Point(x,y), dir)
 
 class SingleAgentSimulation(Simulation):
   def __init__(self) -> None:
@@ -62,7 +65,7 @@ class SingleAgentSimulation(Simulation):
       'path': dpg.generate_uuid(),
       'agents': dpg.generate_uuid()
     }
-    self._agent_ref = dpg.generate_uuid()
+    self._agent_ref: Union[int, str] = dpg.generate_uuid()
     self.simulation_title = "Single Agent simulation"
     self._cell_size = Point(20, 20)
     self._cell_center_x_offset: float = self._cell_size.x/2
@@ -70,34 +73,33 @@ class SingleAgentSimulation(Simulation):
     self._agent: Agent = Agent() # Create an agent at (0,0)
     self._path: AgentPath = [
       # Walk 5 steps East.
-      s(9,4), s(10,4), s(11,4), s(12,4), s(13,4), s(14,4),
+      s(9,4, Direction.EAST), s(10,4), s(11,4), s(12,4), s(13,4), s(14,4),
       # Walk 3 steps south
-      s(14, 5), s(14, 6), s(14, 7),
+      s(14, 5, Direction.SOUTH), s(14, 6), s(14, 7),
       # Walk 6 steps to the East
-      s(15, 7), s(16, 7), s(17, 7), s(18, 7), s(19, 7), s(20, 7),
+      s(15, 7, Direction.EAST), s(16, 7), s(17, 7), s(18, 7), s(19, 7), s(20, 7),
       # Walk 2 steps south
-      s(20, 8), s(20, 9),
+      s(20, 8, Direction.SOUTH), s(20, 9),
       # Walk 8 steps to the West
-      s(19, 9), s(18, 9), s(17, 9), s(16, 9), s(15, 9), s(14, 9), s(13, 9), s(12, 9),
+      s(19, 9, Direction.WEST), s(18, 9), s(17, 9), s(16, 9), s(15, 9), s(14, 9), s(13, 9), s(12, 9),
       ## Walk North 3 steps
-      s(12, 8), s(12, 7), s(12, 6), 
+      s(12, 8, Direction.NORTH), s(12, 7), s(12, 6), 
       # Walk West 3 steps
-      s(11, 6), s(10, 6), s(9, 6),
+      s(11, 6, Direction.WEST), s(10, 6), s(9, 6),
       # Walk North
-      s(9, 5)
+      s(9, 5, Direction.NORTH)
     ]
     self._agent.movement_strategy(build_path_walker(self._path))
 
   def _sim_loop(self, **args):
     """The thread callback that processes a simulation tick."""
     # For now, just have the agent step through a path.
-    current_step_index: int = 0
     while self.simulation_state is not SimulationState.ENDED:
       sleep(self._sim_run_rate) 
       if self.simulation_state is SimulationState.RUNNING:
         self._agent.explore()
-        location_on_grid = self._agent.location.multiply(self._cell_size)
-        dpg.apply_transform(item=self._agent_ref, transform=dpg.create_translation_matrix(tuple(location_on_grid)))
+        self._update_agent_in_scene_graph(self._agent, self._agent_ref)
+
 
   def _toggle_layer(self, sender, item_data, user_data):
     if user_data:
@@ -186,11 +188,11 @@ class SingleAgentSimulation(Simulation):
 
       with dpg.draw_layer(tag=self._layers['agents']): # Agents
         with dpg.draw_node(tag=self._agent_ref):
-          # Draw the triangle centered at cell (0,0) in the grid.
+          # Draw the triangle centered at cell (0,0) in the grid and pointing EAST.
           dpg.draw_triangle(
             p1=(agent_width_half,0), 
-            p2=(0, agent_height), 
-            p3=(agent_width, agent_height), 
+            p2=(-agent_width_half, -agent_height_half), 
+            p3=(-agent_width_half, agent_height_half), 
             color=agent_stroke_color, 
             fill=agent_fill_color, 
             thickness=agent_stroke_thickness
@@ -198,19 +200,43 @@ class SingleAgentSimulation(Simulation):
 
     first_step = self._path[0]
     first_step.perform(self._agent)
-    location_on_grid = self._agent.location.multiply(self._cell_size)
+    self._update_agent_in_scene_graph(self._agent, self._agent_ref)
+
+  def _update_agent_in_scene_graph(self, agent: Agent, node_ref: Union[int, str]) -> None:
+    """
+    Updates a given agent in the scene graph. This effectively causes a re-render.
+
+    Parameters
+    - agent: The agent to update in the scene graph.
+    - node_ref: The DPG reference (tag id) for the node containing the agent in the scene graph.
+    """
+    # 1. Build a matrix for rotating the agent to be in the direction it's facing.
+    rotate = dpg.create_rotation_matrix(DIR_ROTATION[agent.facing], (0,0,-1))
     
-    dpg.apply_transform(
-      item=self._agent_ref, 
-      transform=dpg.create_translation_matrix(tuple(location_on_grid))
-    )
+    # 2. Create a matrix for shifting from being centered at (0,0) to being in a terrain cell.
+    shift_from_origin_to_cell = dpg.create_translation_matrix((10,10))
+
+    # 3. Find the target location on terrain by projecting from cell location to 
+    #    the canvas space.
+    location_on_grid = agent.location.multiply(self._cell_size)
+
+    # 4. Build a matrix for shifting from the first cell (0,0) to the target cell.
+    translate = dpg.create_translation_matrix(tuple(location_on_grid))
+  
+    # 5. Build an affine transformation matrix by multiplying the transformation 
+    #    and rotation matrices together.
+    # Note: The affect of the cumulative transforation is calculated right to left.
+    # So, the rotation happens, then the shift to the first cell, then the shift to 
+    # the target cell.
+    affine_transformation_matrix = translate * shift_from_origin_to_cell * rotate
+    
+    # 6. Apply the transformation to the node in the scene graph containing the agent.
+    dpg.apply_transform(item=node_ref, transform=affine_transformation_matrix)
     
 """
 Closure for an agent walking a path.
 Questions
 - What file should this live in?
-- How do I really want to define a path? List of Points, List of Tuples?, More details: step, turn?
-  Tuples are easy to define, but it's a case of writing to the feature, not an abstraction.
 """
 def build_path_walker(path_to_walk: AgentPath, starting_step_index=-1):
   """A closure that enables an agent to traverse a list of points."""
