379fe255ef65353e4571f1837072ccf7ea357bbc
Samuel Holloway <samuel.holloway@salesforce.com>
2022-01-12 08:32:52 -0600

    Added a TODO.

--- agents_playground/core/simulation.py
+++ agents_playground/core/simulation.py
@@ -281,5 +281,6 @@ def render_stats(**data) -> None:
   """
   context: SimulationContext = data['context']
   stats = context.stats
+  # TODO Need to make the stats text display on top of the terrain.
   dpg.draw_text(tag=stats.fps.id, pos=(20,20), text=f'Frame Rate (Hz): {stats.fps.value}', size=13)
   dpg.draw_text(tag=stats.utilization.id, pos=(20,40), text=f'Utilization (%): {stats.utilization.value}', size=13)
