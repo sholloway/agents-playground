9b53dcea71d4b7d2e4173fc682d5e94f235ab900
Samuel Holloway <sholloway@gmail.com>
2022-08-17 13:04:50 -0500

    Flushing out the town.

--- agents_playground/renderers/our_town_renderers.py
+++ agents_playground/renderers/our_town_renderers.py
@@ -7,16 +7,14 @@ from agents_playground.scene.scene import Scene
 from agents_playground.simulation.context import SimulationContext
 
 
-def ot_building_renderer(self, context: SimulationContext) -> None:
+def building_renderer(self, context: SimulationContext) -> None:
   # Need to convert the building's width, height, and location values into pmin/pmax.
-  min_x, min_y = 10, 400
-  max_x, max_y = 300, 10
-  lower_left_corner = (min_x, min_y)
-  upper_right_corner = (max_x, max_y)
+  # Location on a building is the upper left corner. DPG has the origin of [0,0]
+  # at the upper left corner of the screen.
 
   dpg.draw_rectangle(
     tag=self.id, 
-    pmin=lower_left_corner,
-    pmax=upper_right_corner,
+    pmin=self.location,
+    pmax=(self.location[0] + self.width, self.location[1] + self.height),
     color=self.color, 
     fill=self.fill)
--- agents_playground/renderers/renderers_registry.py
+++ agents_playground/renderers/renderers_registry.py
@@ -6,11 +6,11 @@ from agents_playground.renderers.path import (
 )
 
 from agents_playground.renderers.circle import simple_circle_renderer
-from agents_playground.renderers.our_town_renderers import ot_building_renderer
+from agents_playground.renderers.our_town_renderers import building_renderer
 
 RENDERERS_REGISTRY: Final[Dict[str, Callable]] = {
   'line_segment_renderer': line_segment_renderer,
   'circular_path_renderer': circle_renderer,
   'simple_circle_renderer': simple_circle_renderer,
-  'ot_building_renderer': ot_building_renderer
+  'ot_building_renderer': building_renderer
 }
