d352267762fc6fed472bc18d61fff6a42e3adec0
Samuel Holloway <sholloway@gmail.com>
2022-08-18 07:39:20 -0500

    Crude text display on buildings.
    Laying out the town.

--- agents_playground/renderers/our_town_renderers.py
+++ agents_playground/renderers/our_town_renderers.py
@@ -2,6 +2,7 @@
 Module that contains renderers for the Out Town simulation.
 """
 import dearpygui.dearpygui as dpg
+from agents_playground.renderers.color import Colors
 
 from agents_playground.scene.scene import Scene
 from agents_playground.simulation.context import SimulationContext
@@ -12,9 +13,18 @@ def building_renderer(self, context: SimulationContext) -> None:
   # Location on a building is the upper left corner. DPG has the origin of [0,0]
   # at the upper left corner of the screen.
 
-  dpg.draw_rectangle(
-    tag=self.id, 
-    pmin=self.location,
-    pmax=(self.location[0] + self.width, self.location[1] + self.height),
-    color=self.color, 
-    fill=self.fill)
+  with dpg.draw_node():  
+    dpg.draw_rectangle(
+      tag=self.id, 
+      pmin=self.location,
+      pmax=(self.location[0] + self.width, self.location[1] + self.height),
+      color=self.color, 
+      fill=self.fill
+    )
+    
+    dpg.draw_text(
+      pos = self.location, 
+      text = self.title, 
+      size=14,
+      color = Colors.black
+    )
