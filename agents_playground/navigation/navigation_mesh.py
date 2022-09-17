05ffb4dc429c8d7b606aa41eecf4d992748575d6
Samuel Holloway <sholloway@gmail.com>
2022-09-17 10:58:30 -0500

    Prevent accidently overwriting junction definitions.

--- agents_playground/navigation/navigation_mesh.py
+++ agents_playground/navigation/navigation_mesh.py
@@ -13,8 +13,11 @@ class NavigationMesh:
     self.__junction_location_index: Dict[Point, Tag] = dict()
 
   def add_junction(self, junction: Junction) -> None:
-    self.__junctions[junction.toml_id] = junction
-    self.__junction_location_index[junction.location] = junction.toml_id
+    if junction.toml_id in self.__junctions:
+      raise Exception(f'The junction {junction.toml_id} is already defined in the navigation mesh.')
+    else:
+      self.__junctions[junction.toml_id] = junction
+      self.__junction_location_index[junction.location] = junction.toml_id
 
   def junctions(self) -> ValuesView:
     return self.__junctions.values()
