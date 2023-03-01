f3527ba4ffedbf3e2aa71fff2b859794a54416ed
Samuel Holloway <sholloway@gmail.com>
2023-03-01 07:41:14 -0600

    Fixed tests.

--- demo/paths/project_pkg/__init__.py
+++ demo/paths/project_pkg/__init__.py
@@ -3,8 +3,8 @@
 Important!!!: 
 Reloading modules only works if they're in the same folder as the __init__.py file.
 """
-import paths.scene
+import project_pkg.scene
 
 import importlib
 def reload():
-  importlib.reload(paths.scene)
+  importlib.reload(project_pkg.scene)
--- demo/pulsing_circle/project_pkg/__init__.py
+++ demo/pulsing_circle/project_pkg/__init__.py
@@ -3,8 +3,8 @@
 Important!!!: 
 Reloading modules only works if they're in the same folder as the __init__.py file.
 """
-import pulsing_circle.scene
+import project_pkg.scene
 
 import importlib
 def reload():
-  importlib.reload(pulsing_circle.scene)
+  importlib.reload(project_pkg.scene)
