358a267d20dcf8298128c800d49026374f1290cb
Samuel Holloway <sholloway@gmail.com>
2023-03-17 07:05:16 -0500

    Stylish test.

--- agents_playground/agents/default_agent.py
+++ agents_playground/agents/default_agent.py
@@ -12,6 +12,7 @@ from agents_playground.agents.agent_spec import (
   AgentStyleLike
 )
 from agents_playground.funcs import map_get_or_raise
+from agents_playground.renderers.color import BasicColors, Color
 
 class NamedAgentState(AgentActionStateLike):
   def __init__(self, name: str) -> None:
@@ -75,7 +76,19 @@ class DefaultAgentState(AgentStateLike):
     self.current_action_state = next_state
 
 class DefaultAgentStyle(AgentStyleLike):
-  ...
+  def __init__(
+    self, 
+    stroke_thickness: float      = 1.0, 
+    stroke_color: Color          = BasicColors.black.value,
+    fill_color: Color            = BasicColors.blue.value,
+    aabb_stroke_color: Color     = BasicColors.red.value,
+    aabb_stroke_thickness: float = 1.0
+  ) -> None:
+    self.stroke_thickness       = stroke_thickness
+    self.stroke_color           = stroke_color
+    self.fill_color             = fill_color
+    self.aabb_stroke_color      = aabb_stroke_color
+    self.aabb_stroke_thickness  = aabb_stroke_thickness
 
 class DefaultAgentIdentity(AgentIdentityLike):
   ...
