f74cb66834a651f90ea3f5c501c6f4dc5c8007b8
Samuel Holloway <sholloway@gmail.com>
2023-07-28 08:19:26 -0500

    Playing around with the memory definition.

--- tests/agents/spec/agent_memory_test.py
+++ tests/agents/spec/agent_memory_test.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
+
 from abc import abstractmethod
 from math import inf as INFINITY
-from typing import Any, Dict, Generic, List, Protocol, Set, TypeVar, cast
+from typing import Any, Callable, Dict, Generic, List, Protocol, Set, TypeVar, cast
 
 from agents_playground.agents.spec.tick import Tick as FrameTick
 from agents_playground.containers.ttl_store import TTLStore
@@ -19,8 +20,6 @@ T = TypeVar('T')
 class Memory(Generic[T]):
   def __init__(self, item: T) -> None:
     super().__init__()
-
-  
 
 Tick = int # Represents one frame in the simulation.
 
@@ -53,6 +52,7 @@ class MemoryBankLike(FrameTick, Protocol):
     return []
 
 class NoMemoryBank(MemoryBankLike):
+  """Represents a memory bank that does nothing."""
   def __init__(self) -> None:
     super().__init__()
     self.name = self.__class__.__name__
@@ -68,6 +68,7 @@ MemoryBankName = str
 _missing_memory_bank = NoMemoryBank()
 
 class AgentMemoryLike(FrameTick, Protocol):
+  """A collection of memory banks."""
   memory_banks: Dict[MemoryBankName, MemoryBankLike]
 
   def tick(self) -> None:
@@ -202,6 +203,45 @@ class FakeWorkingMemoryBank:
   pass
 
 
+"""
+What is the contract for a generic monad?
+Two main components:
+Unit : Take a value and turn it into a Monad
+Bind : Take a Monad, apply a function to it and return a new Monad
+
+Monads are contains for values that enable binding functions to 
+them.
+
+Is Memory a good fit for a monad?
+For a memory, I need a generic container that can bind things like
+- expire_on_ttl
+- enable_skill_on_ttl
+
+In this case a skill is a memory with a Time To Learn (ttl)
+skill = Memory(value=CutWood, ttl=5).bind(enable_skill_on_ttl)
+
+A fact is something an Agent knows to be true about it's world.
+This could be used to drive agent behavior as an agent learns
+more about it's world. Or as a game mechanic.
+class WorldFacts(Enum):
+  SkyIsBlue: int = auto()
+fact = Memory(value=WorldFacts.SkyIsBlue)
+"""
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+class Monad(Generic[A]):
+  def __init__(self, value: A):
+    self._value = value
+
+  def bind(self, func: Callable[[A], B]) -> Monad[B]:
+    result = func(self._value)
+    return Monad[B](result)
+    
+  def unwrap(self) -> A:
+    return self._value
+
 class TestAgentMemory:
   def test_extensibility(self) -> None:
     dumb_agent = FakeAgent(memory = NoMemory())
@@ -211,41 +251,59 @@ class TestAgentMemory:
         initial_memory_banks=[StackMemoryBank()]
       )
     )
-    three_tier_agent = DefaultAgentMemory(
-      initial_memory_banks=[
-        FakeSensoryMemoryBank(),
-        # FakeWorkingMemoryBank(),
-        # FakeLongTermMemory()
-      ]
+    three_tier_agent = FakeAgent(
+      memory=DefaultAgentMemory(
+        initial_memory_banks=[
+          FakeSensoryMemoryBank(),
+          # FakeWorkingMemoryBank(),
+          # FakeLongTermMemory()
+        ]
+      )
     )
 
+    agent: FakeAgent
+    memory_bank: MemoryBankLike
+    # for agent in (dumb_agent, default_agent, push_down_automata, three_tier_agent):
+    #   for memory_bank in agent.memory.memory_banks:
+    #     memory_bank.remember() 
   
 """
 Ultimately what am I trying to accomplish?
 - Allow easily replacing the entire agent's memory model.
 - Enable using different storage mechanisms for memory.
 - Enable storing memories with TTLs. 
-- Enable running log when memories expire.
+- Enable running logic when memories expire.
 - Easily checking if a memory exists. 
 - Support different types of memories (senses, facts, skills, states).
 - Partitioning of memory (sensory, working, long term)
 
+Design Goals:
+- Have memories be strongly typed. Avoid the use of Any.
+- Support the ability to have categories of memories.
+  Sense, Recognition, Skill, Fact, etc...
+- Make it easy to create memory banks with different storage 
+  mechanisms.
+
 Current List of Challenges
-- What is a memory? Perhaps we need a memory container class. 
+- What is a memory? 
+  Perhaps we need a memory container class. 
   This is to enable things like sensations, recognitions, skills, 
   facts. 
   Monad example: https://github.com/jasondelaat/pymonad/blob/release/pymonad/monad.py
 
+  
+
   Look at the school of common monads for inspiration.
   Consider generics.
 
-- The contract for MemoryBankLike doesn't work depending on the 
+- What is the contract for remembering something?
+  The contract for MemoryBankLike doesn't work depending on the 
   internal storage mechanism. Parameters like ttl and label don't 
   make sense for most storage types. 
   This is where I wonder if monads and functors could simplify things.
 
-- How should memory retrieval work? Need to have a generic 
-  enough contract to support internal lists, dicts, trees, etc.
+- How should memory retrieval work? 
+  Need to have a generic enough contract to support internal lists, dicts, trees, etc.
 
 - How to deal with multi-storage memory bank like long term memory (memories, skills, knowledge)?
 Options
