089482f04ab1527915c82d0ba45f77b5c64810ed
Samuel Holloway <sholloway@gmail.com>
2023-09-22 07:21:18 -0500

    Monkeying around with Sizers.

--- poc/pyside_webgpu/pyside_webgpu/demos/obj/ui.py
+++ poc/pyside_webgpu/pyside_webgpu/demos/obj/ui.py
@@ -5,20 +5,22 @@ class AppWindow(wx.Frame):
   """Create a simple window with a canvas that can be rendered in."""
   def __init__(self) -> None:
     super().__init__(None, title="Obj Model Loader")
-    self.SetSize(640, 480)
+    self.SetSize(800, 800)
     
+    panel = wx.Panel(self)
 
     """
     Use slides to control the position of the camera.
     wx.SpinButton or SpinCtrl might also be useful.
     """
+
     self.camera_pos_label = wx.StaticText(
-      self, 
+      panel, 
       id     = -1, 
       label  = 'Camera Position'
     )
     self.slider = wx.Slider(
-      self, 
+      panel, 
       id       = -1,
       value    = 0,
       minValue = -1,
@@ -27,15 +29,18 @@ class AppWindow(wx.Frame):
       style    = wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS
     )
 
-    self.canvas = WgpuWidget(self)
+    self.canvas = WgpuWidget(panel)
+    self.canvas.SetMinSize((640, 640))
 
-    sizer = wx.BoxSizer(wx.VERTICAL)
-    sizer.Add((1,20))
-    sizer.Add(self.camera_pos_label, 0, wx.LEFT, 20)
-    sizer.Add((1,10))
-    sizer.Add(self.slider, 0, wx.LEFT, 20)
-    sizer.Add((1,10))
-    sizer.Add(self.canvas, 0, wx.LEFT, 20)
+    top_level_sizer = wx.BoxSizer(wx.VERTICAL)
+    top_level_sizer.Add((1,20))
+    top_level_sizer.Add(self.camera_pos_label, 0, wx.LEFT, 20)
+    top_level_sizer.Add((1,10))
+    top_level_sizer.Add(self.slider, 0, wx.LEFT, 20)
+    top_level_sizer.Add((1,10))
+    top_level_sizer.Add(self.canvas, 0 )
+    panel.SetSizer(top_level_sizer)
 
-    self.SetSizer(sizer)
+    # self.SetAutoLayout(True)
+    
     self.Show()
