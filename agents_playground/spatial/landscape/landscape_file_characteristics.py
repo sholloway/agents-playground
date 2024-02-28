a468ddd2e64061f2ca6f001da647fe8fc687a839
Samuel Holloway <sholloway@gmail.com>
2024-02-28 11:56:11 -0600

    Starting to flush out the schema for landscape files.

--- agents_playground/spatial/landscape/landscape_file_characteristics.py
+++ agents_playground/spatial/landscape/landscape_file_characteristics.py
@@ -5,8 +5,11 @@ from agents_playground.uom import DateTime
 
 @dataclass
 class LandscapeFileCharacteristics:
+  """
+  Metadata about a landscape file.
+  """
   author: Maybe[str] # Can be any string but intended to be First Name Last Name 
   license: Maybe[str] # The license type for this attribute. 
   contact: Maybe[str] # Contact information. Could be anything.
-  creation_time: Maybe[DateTime] # 2024-01-21 hh:mm:ss
-  updated_time: Maybe[DateTime] #YYYY-MM-DD hh:mm:ss
+  creation_time: Maybe[DateTime] # When the file was initially created in the format YYYY-MM-DD hh:mm:ss.
+  updated_time: Maybe[DateTime] # When the file was last updated in the format YYYY-MM-DD hh:mm:ss.
