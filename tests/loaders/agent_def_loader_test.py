612f5cd5bccada3b3fee376a5b37bf3d295f0a76
Samuel Holloway <sholloway@gmail.com>
2024-03-25 12:22:42 -0500

    More progress on defining the contract for agent definitions.

--- tests/loaders/agent_def_loader_test.py
+++ tests/loaders/agent_def_loader_test.py
@@ -1,7 +1,9 @@
+import os
+from pathlib import Path
 from jsonschema import ValidationError
 import pytest
 
-from agents_playground.loaders import LoadSchemaIntoMemory, ValidateJSONWithSchema, ValidateSchema
+from agents_playground.loaders import JSONFileLoader, LoadSchemaIntoMemory, ValidateJSONWithSchema, ValidateSchema
 from agents_playground.loaders.agent_definition_loader import AGENT_DEF_SCHEMA_PATH
 
 class TestAgentDefLoader:
@@ -40,3 +42,11 @@ class TestAgentDefLoader:
     with pytest.raises(ValidationError) as err:
       ValidateJSONWithSchema().process(context, AGENT_DEF_SCHEMA_PATH, file_path='')
     assert "'view_frustum' is a required property" in str(err.value)
+
+  def test_json_valid_full_example(self) -> None:
+    scene_rel_path = "agents_playground/agents/file/agent_def.example.json"
+    agent_def_path = os.path.join(Path.cwd(), scene_rel_path)
+    context = {}
+    jfl = JSONFileLoader()
+    assert jfl.load(context, AGENT_DEF_SCHEMA_PATH, agent_def_path)
+    assert context['json_content'] is not None 
