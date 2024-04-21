1a327b04f5b209dc69d26c679deed8616dcf13e5
Samuel Holloway <sholloway@gmail.com>
2024-04-21 09:18:41 -0500

    Split the New Simulation module into multiple smaller modules.

---
+++ agents_playground/ui/components/input_error_popup.py
@@ -0,0 +1,42 @@
+import wx
+
+class InputErrorPopup(wx.PopupTransientWindow):
+  """
+  Dynamically creates a popup pane that displays an error adjacent to the 
+  parent component. 
+
+  **Example Use**
+  error_popup = InputErrorPopup(
+    parent    = component, 
+    style     = wx.NO_BORDER, 
+    error_msg = self._error_msg,
+    font      = component.GetFont()
+  )
+
+  component_pos = component.ClientToScreen( (0,0) )
+  component_size =  component.GetSize()
+  error_popup.Position(component_pos, (0, component_size[1]))
+  error_popup.Popup()
+
+  """
+  def __init__(self, parent: wx.Window, style: int, error_msg: str, font: wx.Font):
+    super().__init__(parent, style)
+    self._panel = wx.Panel(self)
+    self._panel.SetBackgroundColour('#FFB6C1')
+
+    error_label = wx.StaticText(self._panel, label='Input Error')
+    error_msg_component = wx.StaticText(self._panel, label=error_msg)
+    
+    error_label.SetFont(font.Bold())
+    error_label.SetForegroundColour('black')
+    error_msg_component.SetFont(font)
+    error_msg_component.SetForegroundColour('black')
+    
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    sizer.Add(error_label, flag = wx.ALL, border = 5)
+    sizer.Add(error_msg_component, flag = wx.ALL, border = 5)
+    
+    self._panel.SetSizer(sizer)
+    sizer.Fit(self._panel)
+    sizer.Fit(self)
+    self.Layout()
---
+++ agents_playground/ui/validators/pattern_validator.py
@@ -0,0 +1,87 @@
+import re 
+import wx
+
+from agents_playground.ui.components.input_error_popup import InputErrorPopup
+from agents_playground.ui.validators import keycode_to_value 
+
+class PatternValidator(wx.Validator):
+  def __init__(
+    self, 
+    pattern: str, 
+    error_msg: str, 
+    background_color: tuple[int,...], 
+    foreground_color: tuple[int,...]
+  ) -> None:
+    super().__init__()
+    self._pattern = pattern
+    self._error_msg = error_msg
+    self._caps_lock_key_down: bool = False
+    self._original_background_color = background_color
+    self._original_text_color = foreground_color
+    self.Bind(wx.EVT_KEY_DOWN, self._handle_key_down)
+    self.Bind(wx.EVT_KEY_UP, self._handle_key_up)
+    self.Bind(wx.EVT_CHAR, self._handle_on_char)
+
+  def Clone(self):
+    return PatternValidator(
+      self._pattern, 
+      self._error_msg, 
+      self._original_background_color, 
+      self._original_text_color
+    )
+
+  def _handle_on_char(self, event: wx.KeyEvent) -> None:
+    """Only allow proper characters when typing."""
+    key_code: int = event.GetKeyCode()
+    key_value: str = keycode_to_value(key_code, event.ShiftDown(), self._caps_lock_key_down)
+    if key_code == wx.WXK_DELETE or re.match(self._pattern, key_value) is not None:
+      event.Skip()
+    return
+  
+  def _handle_key_down(self, event: wx.KeyEvent) -> None:
+    keycode = event.GetKeyCode()
+    if keycode == wx.WXK_CAPITAL:
+      self._caps_lock_key_down = True
+    event.Skip()
+    return 
+  
+  def _handle_key_up(self, event: wx.KeyEvent) -> None:
+    keycode = event.GetKeyCode()
+    if keycode == wx.WXK_CAPITAL:
+      self._caps_lock_key_down = False
+    event.Skip()
+    return 
+
+  def Validate(self, win):
+    """Called when the parent component does a self.Validate()"""
+    component = self.GetWindow()
+    value = component.GetValue()
+    match = re.match(self._pattern, value)
+    if match:
+      component.SetBackgroundColour(self._original_background_color) 
+      component.SetForegroundColour(self._original_text_color) 
+      component.Refresh()
+    else:
+      error_popup = InputErrorPopup(
+        parent    = component, 
+        style     = wx.NO_BORDER, 
+        error_msg = self._error_msg,
+        font      = component.GetFont()
+      )
+
+      component_pos = component.ClientToScreen( (0,0) )
+      component_size =  component.GetSize()
+      error_popup.Position(component_pos, (0, component_size[1]))
+      error_popup.Popup()
+
+      component.SetForegroundColour('black')
+      component.SetBackgroundColour('pink')
+      component.SetFocus()
+      component.Refresh()
+    return match is not None
+  
+  def TransferToWindow(self):
+    return True 
+
+  def TransferFromWindow(self):
+    return True 
