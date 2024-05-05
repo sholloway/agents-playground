5332cc90ceb1a69cf3d8ce58341f6f5fd6f1575b
Samuel Holloway <sholloway@gmail.com>
2024-05-05 09:58:27 -0500

    Rotate around an arbitrary axis.

--- agents_playground/spatial/matrix/transformation.py
+++ agents_playground/spatial/matrix/transformation.py
@@ -9,14 +9,101 @@ from agents_playground.spatial.matrix.matrix import Matrix
 from agents_playground.spatial.matrix.matrix4x4 import Matrix4x4, m4
 from agents_playground.spatial.types import Degrees
 from agents_playground.spatial.vector import vector
-from agents_playground.spatial.vector.vector import Vector
+from agents_playground.spatial.vector.vector import VECTOR_ROUNDING_PRECISION, Vector
 
+def rotate_around(
+  point_to_rotate: tuple[float,...], 
+  rotation_point: tuple[float, ...], 
+  axis: Vector,
+  angle: Degrees
+) -> tuple[float, ...]:
+  """Convenance function to rotate a point around a vector.
+
+  This function is equivalent to 
+  t = Transformation()
+  t.rotate_around(rotation_point, axis, angle)
+  new_location = t.transform() * point_to_rotate
+
+  Parameters:
+    point_to_rotate: The point that shall be rotated.
+    rotation_point: The origin of the vector to rotate around. 
+    axis: The vector to rotate the point around.
+    angle: The rotation amount specified in degrees.
+
+  Returns:
+  The new location of the point.
+  """
+  # Source: https://sites.google.com/site/glennmurray/Home/rotation-matrices-and-formulas
+  # Establish aliases for the point to rotate's components.
+  x = point_to_rotate[0]
+  y = point_to_rotate[1]
+  z = point_to_rotate[2]
+
+  # Establish aliases for the point to rotate around's components.
+  a = rotation_point[0]
+  b = rotation_point[1]
+  c = rotation_point[2]
+
+  # Ensure that the axis has a length of 1.
+  axis_norm = axis.unit()
+
+  # Establish aliases for the vector to rotate around's components.
+  u = axis_norm.i
+  v = axis_norm.j
+  w = axis_norm.k
+  
+  # Calculate the trig functions.
+  rads = radians(angle)
+  cosine = round(cos(rads), VECTOR_ROUNDING_PRECISION)
+  one_minus_cosine = 1 - cosine
+  sine = round(sin(rads), VECTOR_ROUNDING_PRECISION)
+
+  # Establish aliases for the various products used in the rotation equations.
+  u_sq = u * u
+  v_sq = v * v
+  w_sq = w * w
+
+  au = a * u 
+  av = a * v
+  aw = a * w
+  
+  bu = b * u 
+  bv = b * v 
+  bw = b * w
+  
+  cu = c * u 
+  cv = c * v 
+  cw = c * w 
+  
+  ux = u * x
+  uy = u * y  
+  uz = u * z 
+  
+  vx = v * x 
+  vy = v * y
+  vz = v * z 
+  
+  wx = w * x 
+  wy = w * y 
+  wz = w * z
+
+  # Evaluate the rotation equations.
+  new_x = (a*(v_sq + w_sq) - u*(bv + cw - ux - vy - wz))*one_minus_cosine + x*cosine + (-cv + bw - wy + vz)*sine
+  new_y = (b*(u_sq + w_sq) - v*(au + cw - ux - vy - wz))*one_minus_cosine + y*cosine + ( cu - aw + wx - uz)*sine
+  new_z = (c*(u_sq + v_sq) - w*(au + bv - ux - vy - wz))*one_minus_cosine + z*cosine + (-bu + av - vx + uy)*sine
+  return (new_x, new_y, new_z)
 
 class Transformation:
   """Convenance class for working with Affine Transformations.
 
   A transformation is a set of affine transformations that are applied 
-  to a vertex or vector in the order that they're added.
+  to a vertex or vector in the order that they're added. 
+
+  Example:
+  To construct a transformation matrix of T = A*B*C:
+  t = Transformation()
+  t.mul(A).mul(B).mul(c)
+  transformation_matrix = t.transform()
   """
   def __init__(self) -> None:
     self._stack: list[Matrix]             = []
@@ -70,6 +157,11 @@ class Transformation:
     )
  
   def rotate_around_x(self, angle: Degrees) -> Self:
+    """Places a rotation matrix on the transformation stack.
+
+    Parameters:
+      angle: An angle in degrees to rotate around the x-axis.
+    """
     rads = radians(angle)
     c = cos(rads)
     s = sin(rads)
@@ -83,6 +175,11 @@ class Transformation:
     )
   
   def rotate_around_y(self, angle: Degrees) -> Self:
+    """Places a rotation matrix on the transformation stack.
+
+    Parameters:
+      angle: An angle in degrees to rotate around the y-axis.
+    """
     rads = radians(angle)
     c = cos(rads)
     s = sin(rads)
@@ -96,26 +193,114 @@ class Transformation:
     )
   
   def rotate_around_z(self, angle: Degrees) -> Self:
+    """Places a rotation matrix on the transformation stack.
+
+    Parameters:
+      angle: An angle in degrees to rotate around the z-axis.
+    """
     rads = radians(angle)
     c = cos(rads)
     s = sin(rads)
     return self.mul(
       m4(
         c, -s, 0, 0,
-        s, c, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1
+        s, c,  0, 0,
+        0, 0,  1, 0,
+        0, 0,  0, 1
       )
     )
     
+  def rotate_around(self,
+    rotation_point: tuple[float, ...], 
+    axis: Vector,
+    angle: Degrees
+  ) -> Self:
+    """Places a rotation matrix on the transformation stack.
 
-  # def rotate(self) -> Matrix:
-  #   """Returns the rotation matrix.
-  #   """
-  #   return m4()
+    Parameters:
+      rotation_point: The origin of the vector to rotate around. 
+      angle: An angle in degrees to rotate.
+      axis: The vector to perform a left-handed rotation around.
+      angle: The rotation amount specified in degrees.
+    """
+    # Source: https://sites.google.com/site/glennmurray/Home/rotation-matrices-and-formulas
+
+    # Establish aliases for the point to rotate around's components.
+    a = rotation_point[0]
+    b = rotation_point[1]
+    c = rotation_point[2]
+
+    # Ensure that the axis has a length of 1.
+    axis_norm = axis.unit()
+
+    # Establish aliases for the vector to rotate around's components.
+    u = axis_norm.i
+    v = axis_norm.j
+    w = axis_norm.k
     
-  
-  # def scale(self) -> Matrix:
-  #   """Returns the scaling matrix.
-  #   """
-  #   return m4()
+    # Calculate the trig functions.
+    rads = radians(angle)
+    cosine = round(cos(rads), VECTOR_ROUNDING_PRECISION)
+    one_minus_cosine = 1 - cosine
+    sine = round(sin(rads), VECTOR_ROUNDING_PRECISION)
+
+    # Establish aliases for the various products used in the rotation equations.
+    u_sq = u * u
+    v_sq = v * v
+    w_sq = w * w
+
+    au = a * u 
+    av = a * v
+    aw = a * w
+    
+    bu = b * u 
+    bv = b * v 
+    bw = b * w
+    
+    cu = c * u 
+    cv = c * v 
+    cw = c * w 
+    
+    uv = u * v
+    uw = u * w 
+    vw = v * w 
+
+    # Evaluate the components of the rotation transformation.
+    m00 = u_sq + (v_sq + w_sq)*cosine 
+    m01 = uv * one_minus_cosine - w * sine
+    m02 = uw * one_minus_cosine + v * sine
+    m03 = (a * (v_sq + w_sq) - u * (bv + cw))*one_minus_cosine + (bw - cv) * sine
+
+    m10 = uv * one_minus_cosine + w * sine
+    m11 = v_sq + (u_sq + w_sq)*cosine 
+    m12 = vw * one_minus_cosine - u * sine 
+    m13 = (b*(u_sq + w_sq) - v*(au + cw))*one_minus_cosine + (cu - aw) * sine 
+
+    m20 = uw * one_minus_cosine - v * sine
+    m21 = vw * one_minus_cosine + u * sine 
+    m22 = w_sq + (u_sq + v_sq) * cosine
+    m23 = (c * (u_sq + v_sq) - w * (au + bv))*one_minus_cosine + (av - bu) * sine
+
+    return self.mul(
+      m4(
+        m00, m01, m02, m03,
+        m10, m11, m12, m13,
+        m20, m21, m22, m23,
+        0,   0,   0,   1
+      ) 
+    )
+
+  def scale(self, v: Vector) -> Self:
+    """Places a translation matrix on the transformation stack.
+
+    Parameters:
+      v: A vector to scale (i.e. stretch or shrink) an item along.
+    """
+    return self.mul(
+      m4(
+        v.i, 0,   0,   0,
+        0,   v.j, 0,   0, 
+        0,   0,   v.k, 0,
+        0,   0,   0,   1
+      )
+    )
--- tests/spatial/transformation_test.py
+++ tests/spatial/transformation_test.py
@@ -2,9 +2,12 @@ import pytest
 
 from math import cos, radians, sin
 
+from agents_playground.spatial.matrix.matrix3x3 import m3
 from agents_playground.spatial.matrix.matrix4x4 import Matrix4x4, m4
+from agents_playground.spatial.types import Degrees
 from agents_playground.spatial.vector import vector
-from agents_playground.spatial.matrix.transformation import Transformation
+from agents_playground.spatial.vector.vector import VECTOR_ROUNDING_PRECISION, Vector
+from agents_playground.spatial.matrix.transformation import Transformation, rotate_around
 
 @pytest.fixture
 def t() -> Transformation:
@@ -95,3 +98,93 @@ class TestTransformation:
       0, 0, 1, 0,
       0, 0, 0, 1
     )
+
+  def test_scale(self, t: Transformation) -> None:
+    x_scale_factor = 1.2
+    y_scale_factor = 1 # No scale
+    z_scale_factor = 4 # 4x
+
+    t.scale(vector(x_scale_factor, y_scale_factor, z_scale_factor))
+
+    assert t.transform() == m4(
+      x_scale_factor, 0, 0, 0,
+      0, y_scale_factor, 0, 0,
+      0, 0, z_scale_factor, 0,
+      0, 0, 0, 1
+    )
+
+  def test_rotate_around_vector_function(self) -> None:
+    point_on_z_axis = (0, 0, 15)
+    point_on_y_axis = rotate_around(
+      point_to_rotate = point_on_z_axis,
+      rotation_point  = (0, 0, 0),       # Rotate around the origin.
+      axis            = vector(1, 0, 0), # rotate around the x-axis.
+      angle           = -90              # Rotate 90 degrees.
+    )
+
+    assert point_on_y_axis == (0, 15, 0)
+
+    negative_point_on_z_axis = rotate_around(
+      point_to_rotate = point_on_y_axis,
+      rotation_point  = (0, 0, 0),       # Rotate around the origin.
+      axis            = vector(1, 0, 0), # rotate around the x-axis.
+      angle           = -90
+    )
+
+    assert negative_point_on_z_axis == (0, 0, -15)
+
+  def test_rotate_around_vector(self, t: Transformation) -> None:
+    t.rotate_around(
+      rotation_point  = (0, 0, 0),       # Rotate around the origin.
+      axis            = vector(1, 0, 0), # rotate around the x-axis.
+      angle           = -90              # Rotate 90 degrees.)
+    )
+    point_on_z_axis = vector(0, 0, 15, 1) # Extend the 3d point to use homogenous coordinates (i.e. w = 1).
+    point_on_y_axis = t.transform() * point_on_z_axis # type: ignore
+    assert point_on_y_axis == vector(0, 15, 0, 1)
+
+    negative_point_on_z_axis = t.transform() * point_on_y_axis
+    assert negative_point_on_z_axis == vector(0, 0, -15, 1)
+
+    # Rotate to the negative y-axis.
+    negative_point_on_y_axis = t.transform() * negative_point_on_z_axis
+    assert negative_point_on_y_axis == vector(0, -15, 0, 1)
+
+    # Rotate to the original position on the z-axis.
+    assert t.transform() * negative_point_on_y_axis == point_on_z_axis
+
+def rotate_around_old(angle, axis):
+  """Places a rotation matrix on the transformation stack.
+
+  Parameters:
+    angle: An angle in degrees to rotate.
+    axis: The vector to perform a left-handed rotation around.
+  """
+  axis = axis.unit() # Enforce that the provided vector is normalized.
+  x = axis.i
+  y = axis.j
+  z = axis.k
+  rads = radians(angle)
+  sine = round(sin(rads), VECTOR_ROUNDING_PRECISION)
+  cosine = round(cos(rads), VECTOR_ROUNDING_PRECISION)
+
+
+  a = cosine + (1.0 - cosine) * (x*x)
+  b = (1.0 - cosine)*(x*y) - x * sine
+  c = (1.0 - cosine)*(x*z) + y*sine
+
+  e = (1.0 - cosine)*(x*y) + z*sine
+  f = cosine + (1.0 - cosine)*(y*y)
+  g = (1.0 - cosine)*(y*z) - x*sine
+  
+  h = (1.0 - cosine)*(x*z) - y*sine
+  i = (1.0 - cosine)*(y*z) + x*sine 
+  j = cosine + (1.0 - cosine)*(z*z)
+
+  return m4(
+    a, b, c, 0,
+    e, f, g, 0,
+    h, i, j, 0,
+    0, 0, 0, 1
+  ) 
+  
